9e324c8862320faf8e016391732ed9447dbaabe9
roby.batas <roby.batas@primecash.co.id>
2024-04-02 23:22:57 +0700

    create:blur_popup_modal

--- lib/src/pages/login.page.dart
+++ lib/src/pages/login.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_bank_app/src/pages/widgets/input_password.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key, required this.title});
@@ -14,68 +15,70 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: null,
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage(
-                'https://th.bing.com/th/id/OIP.Zj8tJS0im56qwmyOKQHRqgHaNK?rs=1&pid=ImgDetMain'),
-            fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                  'https://ik.imagekit.io/fdu5ptj23co/Desain%20tanpa%20judul%20(2)%20(1)_XFkXrqPEd.png'),
+              fit: BoxFit.fill,
+            ),
           ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const SizedBox(height: 400),
-              ElevatedButton.icon(
-                onPressed: () {
-                  setState(() {
-                    _showBlurDialog(context);
-                  });
-                },
-                icon: const Icon(Icons.face),
-                label: const Text('Login'),
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: Colors.blue,
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 100, vertical: 15),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  elevation: 10,
-                  shadowColor:
-                      const Color.fromARGB(255, 243, 244, 245).withOpacity(0.1),
-                  textStyle: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 550),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    setState(() {
+                      _showBlurDialog(context);
+                    });
+                  },
+                  icon: const Icon(Icons.face),
+                  label: const Text('Login'),
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.blue,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 100, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    elevation: 10,
+                    shadowColor: const Color.fromARGB(255, 243, 244, 245)
+                        .withOpacity(0.1),
+                    textStyle: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 30),
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    _buildMenuIcon(
-                        Icons.attach_money, 'e-money', Colors.orange),
-                    const SizedBox(width: 10),
-                    _buildMenuIcon(
-                        Icons.flash_on_outlined, 'Quick Pick', Colors.yellow),
-                    const SizedBox(width: 10),
-                    _buildMenuIcon(Icons.qr_code_2_outlined, 'QR Bayar',
-                        Colors.blueAccent),
-                    const SizedBox(width: 10),
-                    _buildMenuIcon(Icons.qr_code_scanner, 'QR Terima Transfer',
-                        Colors.blue),
-                    const SizedBox(width: 10),
-                    _buildMenuIcon(
-                        Icons.money_outlined, 'Setor Tarik', Colors.purple),
-                  ],
+                const SizedBox(height: 30),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      _buildMenuIcon(
+                          Icons.attach_money, 'e-money', Colors.orange),
+                      const SizedBox(width: 10),
+                      _buildMenuIcon(
+                          Icons.flash_on_outlined, 'Quick Pick', Colors.yellow),
+                      const SizedBox(width: 10),
+                      _buildMenuIcon(Icons.qr_code_2_outlined, 'QR Bayar',
+                          Colors.blueAccent),
+                      const SizedBox(width: 10),
+                      _buildMenuIcon(Icons.qr_code_scanner,
+                          'QR Terima Transfer', Colors.blue),
+                      const SizedBox(width: 10),
+                      _buildMenuIcon(
+                          Icons.money_outlined, 'Setor Tarik', Colors.purple),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -84,49 +87,10 @@ class _LoginPageState extends State<LoginPage> {
 
   void _showBlurDialog(BuildContext context) {
     showDialog(
-      barrierColor: Colors.white.withOpacity(0.3),
+      barrierColor: Colors.white.withOpacity(0.4),
       context: context,
       builder: (BuildContext context) {
-        return Dialog(
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                  hintText: 'Enter text...',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  // Add your button's onPressed logic here
-                },
-                child: const Text('Submit'),
-              ),
-            ],
-          ),
-        );
-        // ElevatedButton(
-        //   onPressed: () {
-        //     Navigator.of(context).pop();
-        //   },
-        //   style: ElevatedButton.styleFrom(
-        //     foregroundColor: Colors.white,
-        //     backgroundColor: Colors.blue,
-        //     padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-        //     shape: RoundedRectangleBorder(
-        //       borderRadius: BorderRadius.circular(50),
-        //     ),
-        //     elevation: 10,
-        //     shadowColor:
-        //         const Color.fromARGB(255, 232, 234, 235).withOpacity(0.1),
-        //     textStyle: const TextStyle(
-        //       fontSize: 20,
-        //       fontWeight: FontWeight.bold,
-        //     ),
-        //   ),
-        //   child: const Text('Login'),
-        // ),
+        return const BlurDialog();
       },
     );
   }
@@ -154,7 +118,7 @@ Widget _buildMenuIcon(
         child: Text(
           label,
           textAlign: TextAlign.center,
-          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
+          style: const TextStyle(fontWeight: FontWeight.normal, fontSize: 12),
         ),
       ),
     ],
---
+++ lib/src/pages/widgets/input_password.dart
@@ -0,0 +1,129 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class BlurDialog extends StatefulWidget {
+  const BlurDialog({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _BlurDialogState createState() => _BlurDialogState();
+}
+
+class _BlurDialogState extends State<BlurDialog> {
+  bool _isPasswordVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: ListView(
+        children: [
+          const SizedBox(height: 30),
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+              padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+              color: Colors.transparent,
+              alignment: Alignment.topCenter,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: IconButton(
+                      icon: Icon(Icons.close),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ),
+                  Image.network(
+                    'https://ik.imagekit.io/fdu5ptj23co/sdas_YLDWAyExuA.png',
+                    height: 100,
+                    width: 100,
+                  ),
+                  const SizedBox(height: 60),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white,
+                      border: Border.all(color: Colors.blue),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 5,
+                          spreadRadius: 2,
+                        ),
+                      ],
+                    ),
+                    child: TextField(
+                      onChanged: (value) {
+                        print(value);
+                      },
+                      obscureText: !_isPasswordVisible,
+                      decoration: InputDecoration(
+                        hintStyle: const TextStyle(
+                          color: Colors.grey,
+                          fontSize: 16,
+                        ),
+                        border: InputBorder.none,
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _isPasswordVisible = !_isPasswordVisible;
+                            });
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Icon(
+                              _isPasswordVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off_rounded,
+                              size: 30,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Handle login button press
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.blue,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 140, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      elevation: 10,
+                      shadowColor: const Color.fromARGB(255, 243, 244, 245)
+                          .withOpacity(0.1),
+                      textStyle: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    child: const Text('Login'),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text('Lupa Password?',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14)),
+                  const SizedBox(height: 30)
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
