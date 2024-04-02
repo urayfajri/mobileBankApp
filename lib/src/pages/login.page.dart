0ff6067472228b95b1a3f81e33541b6c4ff834a9
roby.batas <roby.batas@primecash.co.id>
2024-04-02 13:43:45 +0700

    test

--- lib/src/pages/login.page.dart
+++ lib/src/pages/login.page.dart
@@ -21,10 +21,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
+      appBar: null,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
