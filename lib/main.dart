b0858698c047ebb0fbe94e94d706b8b94ff2eb63
WDTI0\uray.widiansyah <uray.widiansyah@primecash.co.id>
2024-04-03 15:17:12 +0700

    feat(scan-page): layouting

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_bank_app/src/pages/login.page.dart';
+import 'package:mobile_bank_app/src/pages/scan.page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginPage(title: 'Flutter Demo Home Page'),
+      home: const ScanPage(),
     );
   }
 }
--- lib/src/pages/scan.page.dart
+++ lib/src/pages/scan.page.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:mobile_bank_app/src/models/result_scan.model.dart';
+import 'package:mobile_bank_app/src/pages/transaction.page.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
-// import 'package:qr_code_scanner/qr_scanner_overlay_shape_.dart';
 
 class ScanPage extends StatefulWidget {
   const ScanPage({super.key});
@@ -20,15 +22,28 @@ class _ScanPageState extends State<ScanPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('QRIS'),
-          centerTitle: true, // Center the title horizontally
+          flexibleSpace: Container(
+            margin: const EdgeInsets.only(top: 30),
+            child: Center(
+              child: Image.asset(
+                'assets/images/qris_logo.png',
+                height: 30, // Adjust the height as needed
+              ),
+            ),
+          ),
           backgroundColor: Colors.white, // Set background color to white
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.settings),
+              color: Colors.grey, // Choose your desired icon
+              onPressed: () {
+                // Add your onPressed logic here
+              },
+            ),
+          ],
         ),
         body: Stack(
           children: <Widget>[
-            Container(
-              color: Colors.blue,
-            ),
             QRView(
                 key: _qrKey,
                 onQRViewCreated: (QRViewController controller) {
@@ -37,40 +52,101 @@ class _ScanPageState extends State<ScanPage> {
                   });
                   controller.scannedDataStream.listen((val) {
                     if (mounted) {
-                      // print(val.code);
-                      _controller!.dispose();
-                      //Navigator.pop(context, val.code);
+                      if (val.code != null) {
+                        _controller!.dispose();
+                        ResultScan result = ResultScan.fromJson(
+                            jsonDecode(val.code ?? '') as Map<String, dynamic>);
+
+                        Navigator.push(
+                            context,
+                            PageRouteBuilder(
+                              transitionDuration:
+                                  const Duration(milliseconds: 600),
+                              transitionsBuilder: (context, animation,
+                                  secondaryAnimation, child) {
+                                var begin = const Offset(0.0, 1.0);
+                                var end = Offset.zero;
+                                var curve = Curves.ease;
+
+                                var tween = Tween(begin: begin, end: end)
+                                    .chain(CurveTween(curve: curve));
+
+                                return SlideTransition(
+                                  position: animation.drive(tween),
+                                  child: child,
+                                );
+                              },
+                              pageBuilder:
+                                  (context, animation, secondaryAnimation) =>
+                                      TransactionPage(
+                                resultScan: result,
+                              ),
+                            ));
+                        //Navigator.pop(context, val.code);
+                      }
                     }
                   });
                 }),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: ButtonBar(
-                alignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  IconButton(
-                      color: Colors.white,
-                      onPressed: () {
-                        setState(() {
-                          _frontCam = !_frontCam;
-                        });
-                        _controller!.flipCamera();
-                      },
-                      icon: Icon(
-                          _frontCam ? Icons.camera_front : Icons.camera_rear)),
-                  IconButton(
-                      color: Colors.white,
-                      onPressed: () {
-                        setState(() {
-                          _flashOn = !_flashOn;
-                        });
-                        _controller!.toggleFlash();
-                      },
-                      icon: Icon(_flashOn ? Icons.flash_on : Icons.flash_off)),
-                ],
+            Positioned(
+              bottom: 10,
+              right: 0,
+              left: 0,
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: ButtonBar(
+                  alignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    TextButton(
+                        onPressed: () {
+                          setState(() {
+                            _frontCam = !_frontCam;
+                          });
+                          _controller!.flipCamera();
+                        },
+                        child:
+                            _buildMenuIcon(Icons.photo_library, Colors.white)),
+                    TextButton(
+                        onPressed: () {
+                          setState(() {
+                            _flashOn = !_flashOn;
+                          });
+                          _controller!.toggleFlash();
+                        },
+                        child: _buildMenuIcon(
+                            _flashOn
+                                ? Icons.flashlight_on
+                                : Icons.flashlight_off,
+                            Colors.white))
+                  ],
+                ),
               ),
             )
           ],
         ));
+  }
+
+  Widget _buildMenuIcon(IconData iconData, Color iconBackgroundColor) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            color: iconBackgroundColor,
+            shape: BoxShape.circle,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Icon(iconData,
+                size: 24, color: const Color.fromARGB(255, 70, 70, 70)),
+          ),
+        ),
+      ],
+    );
+  }
+
+  // ignore: unused_element
+  Future _openScanPage(BuildContext context) async {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (c) => const ScanPage()));
   }
 }
