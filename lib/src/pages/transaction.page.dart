1b953ede037d3c6ebadf2edc910705893a7e5347
WDTI0\uray.widiansyah <uray.widiansyah@primecash.co.id>
2024-04-04 13:35:27 +0700

    feat(transaction-page): implement logic tap selected balance

--- lib/src/pages/transaction.page.dart
+++ lib/src/pages/transaction.page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
@@ -17,96 +18,233 @@ class TransactionPage extends StatefulWidget {
 }
 
 class _TransactionPageState extends State<TransactionPage> {
+  final TextEditingController _controller = TextEditingController();
   final ResultScan resultScan;
+
+  String _inputNominalTransaction = '';
+  bool _isCreditCardBalanceClicked = false;
+  bool _isSavingClicked = false;
 
   _TransactionPageState({required this.resultScan});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            // Set background color to white
-            ),
-        body: Stack(
-          children: <Widget>[
-            Align(
-              alignment: Alignment.topCenter,
-              child: Column(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 4),
-                    decoration: const BoxDecoration(
-                      color: Color.fromARGB(255, 202, 200, 200),
-                      shape: BoxShape.circle,
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Text(
-                        getInitialWord(resultScan.name),
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(255, 122, 121, 121)),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 4),
+        appBar: AppBar(),
+        body: Container(
+          color: const Color.fromARGB(255, 243, 243, 243),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: Container(
+                  decoration: BoxDecoration(
                     color: Colors.white,
-                    child: Text(
-                      resultScan.name,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
+                    boxShadow: [
+                      BoxShadow(
+                        color: const Color.fromARGB(255, 196, 196, 196)
+                            .withOpacity(0.3), // Shadow color
+                        spreadRadius: 3, // Spread radius
+                        blurRadius: 3, // Blur radius
+                        offset:
+                            const Offset(0, 3), // Offset in x and y direction
                       ),
-                    ),
+                    ],
                   ),
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 4),
-                    child: Text(
-                      resultScan.city,
-                      style: const TextStyle(
-                          fontWeight: FontWeight.w400, color: Colors.grey),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 24),
-                    child: Text(
-                      'Merchant PAN ${resultScan.kodepan}',
-                      style: const TextStyle(
-                          fontWeight: FontWeight.w300, color: Colors.grey),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 24),
-                    child: TextField(
-                        keyboardType: TextInputType.number,
-                        inputFormatters: [
-                          FilteringTextInputFormatter.digitsOnly,
-                          CurrencyInputFormatter(),
-                        ],
-                        onChanged: (value) {},
-                        //editing controller of this TextField
-                        style: const TextStyle(
-                            fontSize: 30, fontWeight: FontWeight.w500),
-                        decoration: const InputDecoration(
-                          labelText: "Nominal Transaksi", //label text of field
-                          labelStyle: TextStyle(
-                              color: Colors.blue,
-                              fontSize: 16), // Change label text color
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Colors.blue), // Change underline color
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 4),
+                        decoration: const BoxDecoration(
+                          color: Color.fromARGB(255, 202, 200, 200),
+                          shape: BoxShape.circle,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Text(
+                            getInitialWord(resultScan.name),
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromARGB(255, 122, 121, 121)),
                           ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Colors
-                                    .blue), // Change underline color when focused
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 4),
+                        color: Colors.white,
+                        child: Text(
+                          resultScan.name,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
                           ),
-                        )),
-                  )
-                ],
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 4),
+                        child: Text(
+                          resultScan.city,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.w400, color: Colors.grey),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 24),
+                        child: Text(
+                          'Merchant PAN • ${resultScan.kodepan}',
+                          style: const TextStyle(
+                              fontWeight: FontWeight.w300, color: Colors.grey),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            right: 24, left: 24, bottom: 32),
+                        child: TextField(
+                            controller: _controller,
+                            keyboardType: TextInputType.number,
+                            inputFormatters: [
+                              FilteringTextInputFormatter.digitsOnly,
+                              CurrencyInputFormatter(),
+                            ],
+                            onChanged: (value) {
+                              setState(() {
+                                _inputNominalTransaction = value;
+                              });
+                            },
+                            //editing controller of this TextField
+                            style: const TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.w500),
+                            decoration: InputDecoration(
+                              labelText:
+                                  "Nominal Transaksi", //label text of field
+                              labelStyle: const TextStyle(
+                                  color: Colors.blue,
+                                  fontSize: 16), // Change label text color
+                              enabledBorder: const UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color:
+                                        Colors.blue), // Change underline color
+                              ),
+                              focusedBorder: const UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Colors
+                                        .blue), // Change underline color when focused
+                              ),
+                              suffixIcon: _inputNominalTransaction.isNotEmpty
+                                  ? IconButton(
+                                      icon: const Icon(Icons
+                                          .clear_rounded), // Icon for clearing input
+                                      onPressed: () {
+                                        // Function to clear input
+                                        _controller.clear();
+                                        _inputNominalTransaction = '';
+                                      },
+                                    )
+                                  : null,
+                            )),
+                      )
+                    ],
+                  ),
+                ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 24,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    const Text(
+                      'Sumber Dana',
+                      style: TextStyle(color: Colors.grey, fontSize: 12),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _isSavingClicked = false;
+                              _isCreditCardBalanceClicked = true;
+                            });
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: _isCreditCardBalanceClicked
+                                  ? const Color.fromARGB(255, 198, 248, 246)
+                                  : Colors.white,
+                              border: Border.all(
+                                color: _isCreditCardBalanceClicked
+                                    ? Colors.blue
+                                    : Colors.grey, // Border color
+                                width: _isCreditCardBalanceClicked ? 1.0 : 0.7,
+                                // Border width
+                              ),
+                              borderRadius: BorderRadius.circular(16.0),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 6),
+                              child: Text(
+                                'Kartu Kredit',
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    color: _isCreditCardBalanceClicked
+                                        ? Colors.blue
+                                        : Colors.black),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 12,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _isSavingClicked = true;
+                              _isCreditCardBalanceClicked = false;
+                            });
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: _isSavingClicked
+                                  ? const Color.fromARGB(255, 198, 248, 246)
+                                  : Colors.white,
+                              border: Border.all(
+                                color: _isSavingClicked
+                                    ? Colors.blue
+                                    : Colors.grey, // Border color
+                                width: _isSavingClicked ? 1.0 : 0.7,
+                                // Border width
+                              ),
+                              borderRadius: BorderRadius.circular(16.0),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 6),
+                              child: Text(
+                                'Tabungan',
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    color: _isSavingClicked
+                                        ? Colors.blue
+                                        : Colors.black),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ));
   }
 }
@@ -145,7 +283,7 @@ class CurrencyInputFormatter extends TextInputFormatter {
     final formatter = NumberFormat.currency(
       locale: 'id_ID',
       decimalDigits: decimalDigits,
-      symbol: 'Rp',
+      symbol: 'Rp ',
     );
     String newText = formatter.format(value);
 
