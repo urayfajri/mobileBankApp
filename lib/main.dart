73c936a3b1c3d28fab4ba816f33f40fb037b510c
WDTI0\uray.widiansyah <uray.widiansyah@primecash.co.id>
2024-04-16 14:25:53 +0700

    feat(splash): animate splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_bank_app/src/pages/login.page.dart';
 
@@ -12,12 +13,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const LoginPage(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Mandiri Living App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: AnimatedSplashScreen(
+          duration: 2000,
+          animationDuration: const Duration(seconds: 1),
+          splash: Image.network(
+            'https://ik.imagekit.io/fdu5ptj23co/f132e326-9f22-4136-83f3-1712e0a223d4_DYZ8c2Are.png',
+          ),
+          splashIconSize: 300,
+          nextScreen: const LoginPage(),
+          splashTransition: SplashTransition.sizeTransition,
+        ));
   }
 }
